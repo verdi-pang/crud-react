7a7e2090e72537de2c249da6171e7abc0a4f9fc4
verdi-pang <75467831+verdi-pang@users.noreply.github.com>
2022-01-19 23:22:35 -0800

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './components/create.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h2 className='main-header'>React CRUD Operations</h2>
+      <div>
+        <Create />
+      </div>
     </div>
   );
 }
